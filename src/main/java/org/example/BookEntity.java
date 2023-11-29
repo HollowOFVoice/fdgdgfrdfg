993e4e99e1782f89bb2144296b644409a9a0cda6
HollowOFVoice <assasinstwo5@gmail.com>
2023-11-29 15:55:23 +0300

    Initial commit

--- src/main/java/org/example/BookEntity.java
+++ src/main/java/org/example/BookEntity.java
@@ -1,19 +1,24 @@
 package org.example;
 
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.concurrent.Flow;
 @Data
+@Getter
 public class BookEntity {
    private String title ;
     private AuthorEntity authors;
     private PublisherEntity publishing;
-    private int year;
+    private String year;
     private String kind;
+    private String id;
+
 
     @Override
     public String toString() {
         return
+              "айди"+  id+'\n'+
         "Название книги: " + title + '\n' +
                 "Автор книги: " + authors +'\n'+
                 "Издатель книги: " + publishing +'\n'+
--- src/main/java/org/example/ListBook.java
+++ src/main/java/org/example/ListBook.java
@@ -1,9 +1,18 @@
 package org.example;
 
 import lombok.Data;
+import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 @Data
+@Getter
 public class ListBook {
     private List<BookEntity> data;
+
+
+    public ListBook() {
+        this.data = new ArrayList<>();
+    }
 }
+
